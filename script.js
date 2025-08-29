7e5b9b72dbebaf3cd47885734c2f78bd2d06cd2c
Badsha012 <badshasheikh361@gmail.com>
2025-08-29 10:11:17 +0600

    nine commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const number='23';
+if(typeof number === 'number'){
+    console.log("Value is a number");
+}
+else{
+    console.log("value is not number");
+}
+
+
+const numbers=[12,78,90];
+console.log(typeof(numbers))
